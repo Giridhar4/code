785fa85638d756bb10288f0bfe238bd0b5f32208
Giridhar4 <giridhar26500@gmail.com>
2024-12-20 18:37:33 -0500

    2 port 4bit

--- dot_S/dec_20.s
+++ dot_S/dec_20.s
@@ -1,61 +1,98 @@
-DDR_A = $6003 ; this and 
-DDR_B = $6002 ; this sets the pin dir to output or input of PORT A and B
-PORT_A = $6001 ; this and 
-PORT_B = $6000 ; this sets the information to PORT A and B
-LCD_RS = %00100000 ; 8ht pin of PORT A is Register select of LCD
-LCD_RW = %01000000 ; read/writre enable 
-LCD_E =  %10000000 ; LCD enble
-LCD_Clear = %00000000
-
-;;;;;;;;;;;;;;;;;;;;;;;;;
   .org $8000  
-  lda #$ff  ; #$ff = #%11111111
-  sta DDR_B ; Set all pins on port B to output
+
+  ; Set DDRs
+  lda #$ff          ; Set all pins on PORT_B to output
+  sta DDR_B         
+
   lda #%11100000
-  sta DDR_A
-;;;;;;;;;;;;;;;;;;;;;;;;;
-; 4-bit data mode for LCD (To save up some pins in the ports) 
-; 0000 - data , 0000 - func
-  lda #%00000000
-  sta PORT_B
+  sta DDR_A          ; Set top three pins on PORT A to output and the rest to input
 
-  lda #%00100010
-  sta PORT_B
-;----------------------
-  lda #%00000000
-  sta PORT_B
+  ; Initialize LCD in 4-bit mode
+  lda #%00000001     ; Clear the display
+  jsr LCD_inst
 
-  lda #%00000010
-  sta PORT_B
+  lda #%00111000     ; Set 4-bit mode (only the high nibble is used)
+  jsr LCD_inst
   
-  ora #%11100000
-  sta PORT_B
+  lda #%00001110     ; Display on, cursor off, blink off
+  jsr LCD_inst
 
-  lda #%00000000
-  sta PORT_B
-;--------------------
-  lda #%00000010
-  sta PORT_B
-  
-  ora #%01100000
-  sta PORT_B
+  lda #%00000110     ; Increment cursor, no display shift
+  jsr LCD_inst
 
-  lda #%00000000
-  sta PORT_B
-;--------------------
-  lda #%01001010
-  sta PORT_B
-  
-  and #%10001010
-  sta PORT_B
-
-  lda #%00000000
-  sta PORT_B
+  ; Print "Murggi Pittu"
+  lda #"M"
+  jsr print_char
+  lda #"u"
+  jsr print_char
+  lda #"r"
+  jsr print_char
+  lda #"g"
+  jsr print_char
+  lda #"g"
+  jsr print_char
+  lda #"i"
+  jsr print_char
+  lda #" "
+  jsr print_char
+  lda #"P"
+  jsr print_char
+  lda #"i"
+  jsr print_char
+  lda #"t"
+  jsr print_char
+  lda #"t"
+  jsr print_char
+  lda #"u"
+  jsr print_char
 
 loop:
   jmp loop
 
+; ---------------------------
+; Subroutines
+; ---------------------------
 
+; LCD_inst: Sends a command in 4-bit mode
+LCD_inst:
+  ; Send high nibble (upper 4 bits)
+  and #%11110000        ; Mask low nibble (bits 0–3)
+  jsr SendNibble         ; Send high nibble
+
+  ; Send low nibble (lower 4 bits)
+  asl                    ; Shift bits left (to move low nibble to high)
+  asl
+  asl
+  asl
+  and #%11110000        ; Mask high nibble (now low nibble is in the upper 4 bits)
+  jsr SendNibble         ; Send low nibble
+
+  rts
+
+; print_char: Sends a character in 4-bit mode
+print_char:
+  ; Set RS for data (RS=1)
+  lda #LCD_RS
+  sta PORT_A
+  jsr SendNibble         ; Send high nibble of the character
+  lda #(LCD_RS | LCD_E)  ; Set RS=1, E=1 (for data)
+  sta PORT_A
+  lda #LCD_RS           ; Set RS=1 (data), E=0
+  sta PORT_A
+  jsr SendNibble         ; Send low nibble of the character
+  rts
+
+; SendNibble: Sends a single 4-bit nibble to the LCD
+SendNibble:
+  sta PORT_B            ; Output nibble to PORT_B
+  lda #LCD_E            ; Set E=1 (Enable)
+  sta PORT_A
+  lda #LCD_Clear        ; Clear the control pins
+  sta PORT_A
+  rts
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+  
   .org $fffc
   .word $8000
   .word $0000
