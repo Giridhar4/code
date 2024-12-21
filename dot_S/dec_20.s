0783e32467e8f6f1420e0e0449c469a8fdd83956
Giridhar4 <giridhar26500@gmail.com>
2024-12-20 20:10:17 -0500

    2 port 4 bit done

--- dot_S/dec_20.s
+++ dot_S/dec_20.s
@@ -1,98 +1,135 @@
-  .org $8000  
+DDR_A = $6003 ; this and 
+DDR_B = $6002 ; this sets the pin dir to output or input of PORT A and B
+PORT_A = $6001 ; this and 
+PORT_B = $6000 ; this sets the information to PORT A and B
+LCD_RS = %00100000 ; 8ht pin of PORT A is Register select of LCD
+LCD_RW = %01000000 ; read/writre enable 
+LCD_E =  %10000000 ; LCD enble
+LCD_Clear = %00000000 ; use when you need to clear all the info in the pins
 
-  ; Set DDRs
-  lda #$ff          ; Set all pins on PORT_B to output
-  sta DDR_B         
+;;;;;;;;;;;;;;;;;;;;;;;;;
+  .org $8000  
+  lda #$ff  ; #$ff = #%11111111
+  sta DDR_B ; Set all pins on port B to output
 
   lda #%11100000
-  sta DDR_A          ; Set top three pins on PORT A to output and the rest to input
-
-  ; Initialize LCD in 4-bit mode
-  lda #%00000001     ; Clear the display
+  sta DDR_A ; Set top three pins on port A to output and rest to input
+;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;jsr LCD_init ; jump to "lcd_init" subroutine -- initilizes the LCD
+  lda #%00100000 ; inst to clear 
+; clears all the info and resets the display
   jsr LCD_inst
 
-  lda #%00111000     ; Set 4-bit mode (only the high nibble is used)
-  jsr LCD_inst
-  
-  lda #%00001110     ; Display on, cursor off, blink off
+  lda #%00000000
   jsr LCD_inst
 
-  lda #%00000110     ; Increment cursor, no display shift
+  lda #%00010000
   jsr LCD_inst
 
-  ; Print "Murggi Pittu"
-  lda #"M"
-  jsr print_char
-  lda #"u"
-  jsr print_char
+  lda #%00100000
+  jsr LCD_inst
+
+  lda #%00000000
+  jsr LCD_inst
+
+  lda #%00000000
+  jsr LCD_inst
+
+  lda #%11100000
+  jsr LCD_inst
+
+  lda #%00000000
+  jsr LCD_inst
+
+  lda #%01100000
+  jsr LCD_inst
+
+  lda #"G"
+  jsr send
+
+  lda #"i"
+  jsr send
+
   lda #"r"
-  jsr print_char
-  lda #"g"
-  jsr print_char
-  lda #"g"
-  jsr print_char
+  jsr send
+
   lda #"i"
-  jsr print_char
+  jsr send
+
+  lda #"d"
+  jsr send
+
+  lda #"h"
+  jsr send
+
+  lda #"a"
+  jsr send
+
+  lda #"r"
+  jsr send
+
   lda #" "
-  jsr print_char
-  lda #"P"
-  jsr print_char
+  jsr send
+
   lda #"i"
-  jsr print_char
-  lda #"t"
-  jsr print_char
-  lda #"t"
-  jsr print_char
-  lda #"u"
-  jsr print_char
+  jsr send
+
+  lda #"s"
+  jsr send
+
+  lda #" "
+  jsr send
+
+  lda #"G"
+  jsr send
+
+  lda #"o"
+  jsr send
+
+  lda #"d"
+  jsr send
 
 loop:
   jmp loop
 
-; ---------------------------
-; Subroutines
-; ---------------------------
 
-; LCD_inst: Sends a command in 4-bit mode
 LCD_inst:
-  ; Send high nibble (upper 4 bits)
-  and #%11110000        ; Mask low nibble (bits 0–3)
-  jsr SendNibble         ; Send high nibble
-
-  ; Send low nibble (lower 4 bits)
-  asl                    ; Shift bits left (to move low nibble to high)
-  asl
-  asl
-  asl
-  and #%11110000        ; Mask high nibble (now low nibble is in the upper 4 bits)
-  jsr SendNibble         ; Send low nibble
-
+  pha
+  sta PORT_B
+  lda #LCD_Clear
+  sta PORT_A
+  lda #LCD_E
+  sta PORT_A
+  lda #LCD_Clear
+  sta PORT_A
+  pla
   rts
 
-; print_char: Sends a character in 4-bit mode
+send:
+  pha
+  jsr print_char
+  asl
+  asl
+  asl
+  asl
+  jsr print_char
+  pla
+  rts
+
+
 print_char:
-  ; Set RS for data (RS=1)
+  pha
+  sta PORT_B
   lda #LCD_RS
   sta PORT_A
-  jsr SendNibble         ; Send high nibble of the character
-  lda #(LCD_RS | LCD_E)  ; Set RS=1, E=1 (for data)
+  lda #(LCD_RS | LCD_E)
   sta PORT_A
-  lda #LCD_RS           ; Set RS=1 (data), E=0
+  lda #LCD_RS
   sta PORT_A
-  jsr SendNibble         ; Send low nibble of the character
+  pla
   rts
 
-; SendNibble: Sends a single 4-bit nibble to the LCD
-SendNibble:
-  sta PORT_B            ; Output nibble to PORT_B
-  lda #LCD_E            ; Set E=1 (Enable)
-  sta PORT_A
-  lda #LCD_Clear        ; Clear the control pins
-  sta PORT_A
-  rts
-
-;;;;;;;;;;;;;;;;;;;;;;;;
-  
-  .org $fffc
-  .word $8000
-  .word $0000
+  .org $fffc  ;since 6502 starts reading from fffc and fffd
+  .word $8000 ; reset is set to start from $8000 look at line 11 to understand 
+  .word $0000 ; serves as padding because the last two bytes are unfilles and should be filled to compleate all 32768 bytes
+  ; only then you can successfully upload on to the eeprom if not it is not possible.
